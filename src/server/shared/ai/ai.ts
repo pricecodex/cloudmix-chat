7f28f0f13ab72c677c210b41d3e74719ecd3fc62
Alimkhan <pricecodex@gmail.com>
2025-08-17 21:04:50 +0300

    chore: minor clean up

--- src/server/shared/ai/ai.ts
+++ src/server/shared/ai/ai.ts
@@ -3,6 +3,4 @@ import { GoogleGenAI } from "@google/genai";
 export const AI_USERNAME = "@ai-chat@";
 export const AI_DEFAULT_MESSAGE = "Hey There!";
 
-export const ai = new GoogleGenAI({
-  apiKey: "AIzaSyDcG7SLVleNctF4wUB_Qk4ujepiPwzLOIc",
-});
+export const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY! });
